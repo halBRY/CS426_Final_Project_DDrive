51f3d19d6082b83c841209e4477bea11d9b2b900
ngardu <ngardu2@uic.edu>
2024-04-01 20:00:11 -0500

    TEst some basic map masking

--- Assets/Scripts/HoldNote.cs
+++ Assets/Scripts/HoldNote.cs
@@ -6,10 +6,12 @@ public class HoldNote : MonoBehaviour
 {
     public AccuracyManager accuracyManager;
     public PlayerController player;
+    public HoldNote startHold;
 
     private bool canHit = false;
     private bool missedNote = false;
     // Start is called before the first frame update
+    private bool hitStart;
     void Start()
     {
         
@@ -22,23 +24,46 @@ public class HoldNote : MonoBehaviour
         {
             if(player.getAttemptHit())
             {
-                Debug.Log("hit hold");
+                Debug.Log("hold hit");
                 accuracyManager.Perfect();
-                Destroy(gameObject);
+                gameObject.GetComponent<Renderer>().enabled = false;
+                hitStart = true;
                 canHit = false;
             }
+        }
+        else if (hitStart && !canHit)
+        {
+            Debug.Log("add a way to keep track of player holding keydown");
         }
     }
 
     void OnTriggerEnter (Collider collider)
     {
-        canHit = true;
+        
+        if(gameObject.tag == "HoldStart")
+        {
+            canHit = true;
+        }
+        else if (getStart() && gameObject.tag == "HoldEnd")
+        {
+                Debug.Log("Hold hit end");
+                accuracyManager.Perfect();
+                Destroy(transform.parent.gameObject);
+        }
+        else if (getStart() && gameObject.tag == "HoldCenter")
+        {
+                Debug.Log("Hold hit center");
+                accuracyManager.Perfect();
+                Destroy(gameObject);
+        }
+     
+
     }
 
     void OnTriggerExit(Collider collider)
     {
         // In if to prevent a note being counted twice if missed
-        if(!missedNote)
+        if(!missedNote && !getStart())
         {
             missedNote = true;
             Destroy(transform.parent.gameObject);
@@ -47,5 +72,10 @@ public class HoldNote : MonoBehaviour
             accuracyManager.ResetCombo();
             player.MissedHit();
         }
+    }
+
+    bool getStart()
+    {
+        return startHold.hitStart;
     }
 }
