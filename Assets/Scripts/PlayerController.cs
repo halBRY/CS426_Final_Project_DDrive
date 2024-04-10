e07036cd5e491b16fdd89854e04fe467fde7a046
oortiz22 <ortizorla0@gmail.com>
2024-04-09 23:06:28 -0500

    added Speedometer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -58,6 +58,19 @@ public class PlayerController : MonoBehaviour
     public float textFadeTime;
     private float speedMod;
 
+    //SPEEDOMETER
+    public Texture2D backTex;
+	public Texture2D dialTex;
+	public Texture2D needleTex;
+	public Texture2D needleCache;
+	public float needleSizeRatio=3;
+	public Vector2 counterPos;
+	public Vector2 counterSize = new Vector2(200,200);
+	public float topSpeed=100;
+	public float stopAngle=-211;
+	public float topSpeedAngle=31;
+    public float speed;
+
     private void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -172,7 +185,7 @@ public class PlayerController : MonoBehaviour
         }
 
         //Calculate player speed
-        float speed = Vector3.Distance(transform.position, lastPos) / Time.deltaTime;
+         speed = Vector3.Distance(transform.position, lastPos) / Time.deltaTime;
         lastPos = transform.position;
         lastRot = transform.rotation.y;
 
@@ -248,5 +261,30 @@ public class PlayerController : MonoBehaviour
         //Debug.Log("Collision Detected");
         //Debug.Log(other.gameObject.tag);
     }
+    void  OnGUI (){
+
+		// Draw the back
+		GUI.DrawTexture( new Rect(counterPos.x, counterPos.y, counterSize.x, counterSize.y), backTex);
+
+		// Draw the counter
+		GUI.DrawTexture( new Rect(counterPos.x+15, counterPos.y+15, counterSize.x-30, counterSize.y-30), dialTex);
+
+		// Calculate center
+		Vector2 centre= new Vector2(counterPos.x + (counterSize.x / 2), counterPos.y + (counterSize.y / 2) );
+		Matrix4x4 savedMatrix= GUI.matrix;
+
+		// Calculate angle
+		float speedFraction= speed / topSpeed;
+		float needleAngle= Mathf.Lerp(stopAngle, topSpeedAngle, speedFraction);
+
+		GUIUtility.RotateAroundPivot(needleAngle, centre);
+
+		// Draw the needle
+		GUI.DrawTexture( new Rect(centre.x+5, (centre.y+10) - needleTex.height / 2, needleTex.width/needleSizeRatio, needleTex.height/needleSizeRatio), needleTex);
+		GUI.matrix = savedMatrix;
+
+		// Draw the needle cache
+		GUI.DrawTexture( new Rect(centre.x-30, centre.y-30, 60, 60), needleCache);
+	}
 
 }
